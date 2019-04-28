a530a3a3ea6142d4e0c90a531ff2f96fa576986b
Mark Carlebach <markjcarlebach@gmail.com>
2019-04-28 13:59:53 -0400

    Very primitive but not showing up as new dag

--- first_challenge.py
+++ first_challenge.py
@@ -1,7 +1,67 @@
-"""
+#########################################
+#        My First Challenge
+#########################################
+#  Create salted set of tasks
+#  a) Create a task dependent on existence of 2 local (or AWS) file
+#  b) Zip contents of the two files together
+#  c) Create output file of combined data where name includes hash of input files
+#  d) Create new file if combined does not exist
+#  e) Create new file if tag in file name does not match salt of previous file
+#########################################
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+# Define common default arguments for all operators in this example DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+
+# Create example DAG to demonstrate salting of file names
+dagFIRST = DAG('first',
+               default_args=default_args,
+               schedule_interval=timedelta(days=1)
+               )
+
+
+# Add first task to DAG we of this example
+t1 = BashOperator(
+    task_id= "echo1",
+    bash_command= "echo 1",
+    dag=dagFIRST)
+
+
+
+# Add first task to DAG we of this example
+t2 = BashOperator(
+    task_id= "echo2",
+    bash_command= "echo 2",
+    dag=dagFIRST)
+
+
+t2.set_upstream(t1)
+
+
+#########################################
+# Old Tutorial Code
+#########################################
+'''
 Code that goes along with the Airflow tutorial located at:
 https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
-"""
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
@@ -52,3 +112,4 @@ t3 = BashOperator(
 
 t2.set_upstream(t1)
 t3.set_upstream(t1)
+'''
