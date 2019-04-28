e408ba361732d70fdd1da23cf289795a33987002
Mark Carlebach <markjcarlebach@gmail.com>
2019-04-28 18:34:21 -0400

    Working

--- dags/first_challenge.py
+++ dags/first_challenge.py
@@ -15,25 +15,19 @@ from datetime import datetime, timedelta
 
 # Define common default arguments for all operators in this example DAG
 default_args = {
-    'owner': 'Mark',
-    'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    #'email': ['airflow@example.com'],
-    #'email_on_failure': False,
-    #'email_on_retry': False,
-    #'retries': 1,
-    #'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    "owner": 'mjcarleb',
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "start_date": datetime(2019, 4, 28),
+    "end_date": datetime(2019,4,29),
+    "depends_on_past": False,
 }
 
 
 # Create example DAG to demonstrate salting of file names
-dagFIRST = DAG('first3',
+dagFIRST = DAG('first4',
                default_args=default_args,
-               schedule_interval="5 * * * *"
+               schedule_interval="05 * * * *"
                )
 
 
