96cc6549867753a11bd97a4e82901ec012f8464e
Mark Carlebach <markjcarlebach@gmail.com>
2019-04-29 15:19:44 -0400

    Working with return of value via xcom

--- dags/XCOM_challenge.py
+++ dags/XCOM_challenge.py
@@ -10,13 +10,16 @@ yday = datetime.combine(datetime.today() - timedelta(1),
                                   datetime.min.time())
 
 
-def print_my_name(my_name):
+def push_via_return():
+    return 10
 
-    print(f"hello, {my_name}")
+def pull_via_return(**context):
 
-    df = pd.read_csv("~/yelp1.csv")
-    print(df.shape)
-    return "Whatever you return gets printed in logs"
+    value1 = context["task_instance"].xcom_pull(task_ids="t_push_via_return")
+
+    print(f"Value pulled via XCOM pull by return (10?) = {value1}")
+
+    return "Finished data_puller_both_ways"
 
 
 default_args = {
@@ -24,51 +27,25 @@ default_args = {
     'depends_on_past': False,
     'start_date': yday,
     'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=5)
+    'retries': 0
 }
 
-dag = DAG('pass_arg_print_name2',
+dag = DAG('demo_XCOM6',
           default_args=default_args,
           schedule_interval='@once')
 
-t1 = S3KeySensor(
-    task_id='detect',
-    poke_interval=0,
-    timeout=10,
-    bucket_key='s3://cscie29-data/pset5/yelp_data/yelp_subset_1.csv',
-    bucket_name=None,
-    aws_conn_id = "aws_default",
-    dag=dag)
-
-t2 = BashOperator(
-    task_id='change_dir',
-    depends_on_past=False,
-    bash_command="cd $AIRFLOW_HOME",
-    dag=dag)
-
-t3 = BashOperator(
-    task_id='touch_as_proof',
-    depends_on_past=False,
-    bash_command="touch ~/iwashere.txt",
-    dag=dag)
-
-t4 = BashOperator(
-    task_id='copy_s3_local',
-    depends_on_past=False,
-    bash_command="aws s3 cp s3://cscie29-data/pset5/yelp_data/yelp_subset_1.csv ~/yelp1.csv",
-    dag=dag)
-
-
-t5 = PythonOperator(
-    task_id = "pandas_python_operator",
+t1 = PythonOperator(
+    task_id = "t_push_via_return",
     provide_context=False,
-    python_callable=print_my_name,
-    op_kwargs={"my_name": "MarkJoseph"},
+    python_callable=push_via_return,
     dag=dag)
 
-t2.set_upstream(t1)
-t3.set_upstream(t2)
-t4.set_upstream(t3)
-t5.set_upstream(t4)
+
+t2 = PythonOperator(
+    task_id = "t_pull_via_return",
+    provide_context=True,
+    python_callable=pull_via_return,
+    dag=dag)
+
+
+t2.set_upstream([t1])
