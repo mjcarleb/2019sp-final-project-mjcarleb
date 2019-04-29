6cd88ecb27a901c12ba49d4f6e16663feee1d813
Mark Carlebach <markjcarlebach@gmail.com>
2019-04-29 13:24:43 -0400

    Starting with dag that passed variables to python operator

---
+++ dags/XCOM_challenge.py
@@ -0,0 +1,74 @@
+from airflow import DAG
+from airflow.operators.sensors import S3KeySensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+import pandas as pd
+
+yday = datetime.combine(datetime.today() - timedelta(1),
+                                  datetime.min.time())
+
+
+def print_my_name(my_name):
+
+    print(f"hello, {my_name}")
+
+    df = pd.read_csv("~/yelp1.csv")
+    print(df.shape)
+    return "Whatever you return gets printed in logs"
+
+
+default_args = {
+    'owner': 'mjcarleb',
+    'depends_on_past': False,
+    'start_date': yday,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('pass_arg_print_name2',
+          default_args=default_args,
+          schedule_interval='@once')
+
+t1 = S3KeySensor(
+    task_id='detect',
+    poke_interval=0,
+    timeout=10,
+    bucket_key='s3://cscie29-data/pset5/yelp_data/yelp_subset_1.csv',
+    bucket_name=None,
+    aws_conn_id = "aws_default",
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='change_dir',
+    depends_on_past=False,
+    bash_command="cd $AIRFLOW_HOME",
+    dag=dag)
+
+t3 = BashOperator(
+    task_id='touch_as_proof',
+    depends_on_past=False,
+    bash_command="touch ~/iwashere.txt",
+    dag=dag)
+
+t4 = BashOperator(
+    task_id='copy_s3_local',
+    depends_on_past=False,
+    bash_command="aws s3 cp s3://cscie29-data/pset5/yelp_data/yelp_subset_1.csv ~/yelp1.csv",
+    dag=dag)
+
+
+t5 = PythonOperator(
+    task_id = "pandas_python_operator",
+    provide_context=False,
+    python_callable=print_my_name,
+    op_kwargs={"my_name": "MarkJoseph"},
+    dag=dag)
+
+t2.set_upstream(t1)
+t3.set_upstream(t2)
+t4.set_upstream(t3)
+t5.set_upstream(t4)
